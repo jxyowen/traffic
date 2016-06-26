1db740f0f774c76ff30a09efa642fa7c6a28f938
jxyowen <jxyowen@qq.com>
2016-06-26 11:29:58 +0800

    user

--- api/models.py
+++ api/models.py
@@ -116,5 +116,5 @@ class VLANModel(models.Model):
         return 'id: %s  mode: %s ' % (self.id, self.mode)
 
 
-admin.site.register(GeneratorModel)
-admin.site.register(SwitchModel)
+# admin.site.register(GeneratorModel)
+# admin.site.register(SwitchModel)
